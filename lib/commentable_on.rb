16f6ff1fb890afef8f2f2532d11ae3ee356830e9
Samuel Ralak <thesamuelralak@gmail.com>
2022-11-16 00:43:58 +0300

    make it so :rocket:

---
+++ lib/commentable_on.rb
@@ -0,0 +1,6 @@
+require "commentable_on/version"
+
+module CommentableOn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/commentable_on/version.rb
@@ -0,0 +1,3 @@
+module CommentableOn
+  VERSION = "0.1.0"
+end

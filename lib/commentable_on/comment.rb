77773707fb8eb760e86cfc37333661c700c388f7
Samuel Ralak <thesamuelralak@gmail.com>
2022-11-18 01:50:36 +0300

    add uuid support for ancestry

--- lib/commentable_on/comment.rb
+++ lib/commentable_on/comment.rb
@@ -2,7 +2,7 @@ require 'ancestry'
 
 module CommentableOn
   class Comment < ::ActiveRecord::Base
-    has_ancestry
+    has_ancestry(primary_key_format: /\A[\w\-]+(\/[\w\-]+)*\z/, ancestry_column: :thread)
 
     if defined?(ProtectedAttributes)
       attr_accessible :commentable_id, :commentable_type, :commenter_id, :commenter_type, :commentable, :commenter, :body
